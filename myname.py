33c591213e16efbebd76ee8111194baf91ceb4a0
kokokokokl <zzh921932450@qq.com>
2016-03-15 16:32:50 +0800

    Added files via upload

---
+++ myname.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 16:07:02 2016
+
+@author: lenovo
+"""
+
+def myname():
+    print("&&&&&&&&     &&&&&&&&      &       &")
+    print("      &            &       &       &")
+    print("     &            &        &       &")
+    print("    &            &         &&&&&&&&&")
+    print("   &            &          &       &")
+    print("  &            &           &       &")
+    print(" &            &            &       &")
+    print("&&&&&&&&     &&&&&&&&      &       &")
+myname()
