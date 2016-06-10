8cfac0f6b3eb0ce684d0cfce2e8feb940f339aac
kokokokokl <zzh921932450@qq.com>
2016-06-10 17:07:50 +0800

    Add files via upload

---
+++ chapter1/人口模型.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 10:23:41 2016
+
+@author: lenovo
+"""
+
+import numpy as np
+from pylab import *
+from math import *
+time=5
+dt=0.001
+t=[]
+for i in range(int(time/dt)+1):
+    t.append(i*dt)
+#numerical solution
+def solve_N(a,b):
+    N=[]
+    t=[]
+    N_0=1000#float(input('N0='))
+    N.append(N_0)
+    t.append(0)
+    time=5#float(input('total time='))
+    dt=0.001#float(input('time step='))
+    for i in range(int(time/dt)):
+        N.append(N[i]+dt*(a*N[i]-b*(N[i])**2))#Attn:It is'**',not'^'
+        t.append(t[i]+dt)
+    return N
+N_0=solve_N(10,0.01)
+
+
+#exact solution
+def exact_N(a,b):
+    N=[]
+    t=[]
+    N_0=100#float(input('N0='))
+    N.append(N_0)
+    t.append(0)
+    time=5#float(input('total time='))
+    dt=0.001#float(input('time step='))
+    for i in range(int(time/dt)):
+        N.append(a*N_0*e**(a*t[i])/(a+b*N_0*(e**(a*t[i])-1)))#Attn:It is'**',not'^'
+        t.append(t[i]+dt)
+    return N
+N_exact0=exact_N(10,0.01)
+
+
+#plot 
+plot(t,N_0,'--',color='red')
+plot(t,N_exact0,color='red')
+legend('b=0 numerical','b=0 exact')
+xlabel('t(year)')
+ylabel('N')
+savefig('problem1.6.png')
+show()
